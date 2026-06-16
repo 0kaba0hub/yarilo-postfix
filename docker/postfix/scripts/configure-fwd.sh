1553a8c5065c6248bd2f55407e1d24ed3312d634
Ihor Rusyn <0kaba0@gmail.com>
2026-06-16 16:43:49 +0200

    fix(postsrsd): milter on 10001 + socketmap on 10003, rename POSTSRSD_ADDR to POSTSRSD_HOST

--- docker/postfix/scripts/configure-fwd.sh
+++ docker/postfix/scripts/configure-fwd.sh
@@ -3,7 +3,7 @@ set -eu
 . /common.sh
 
 RSPAMD_ADDR="${RSPAMD_ADDR:-localhost:11332}"
-POSTSRSD_ADDR="${POSTSRSD_ADDR:-yarilo-postsrsd:10003}"
+POSTSRSD_HOST="${POSTSRSD_HOST:-yarilo-postsrsd}"
 LMTP_ADDR="${LMTP_ADDR:-}"
 
 postconf -e "relayhost ="
@@ -14,9 +14,9 @@ if [ -n "${LMTP_ADDR}" ]; then
     postconf -e "lmtp_host_lookup = native"
 fi
 
-postconf -e "sender_canonical_maps = socketmap:inet:${POSTSRSD_ADDR}:forward"
+postconf -e "sender_canonical_maps = socketmap:inet:${POSTSRSD_HOST}:10003:forward"
 postconf -e "sender_canonical_classes = envelope_sender"
-postconf -e "recipient_canonical_maps = socketmap:inet:${POSTSRSD_ADDR}:reverse"
+postconf -e "recipient_canonical_maps = socketmap:inet:${POSTSRSD_HOST}:10003:reverse"
 postconf -e "recipient_canonical_classes = envelope_recipient,header_recipient"
 
 postconf -e "smtpd_relay_restrictions = permit_mynetworks reject_unauth_destination"
@@ -24,5 +24,5 @@ postconf -e "smtpd_recipient_restrictions = permit_mynetworks, reject_unauth_des
 
 postconf -e "milter_default_action = accept"
 postconf -e "milter_protocol = 6"
-postconf -e "smtpd_milters = inet:${RSPAMD_ADDR}"
-postconf -e "non_smtpd_milters = inet:${RSPAMD_ADDR}"
+postconf -e "smtpd_milters = inet:${RSPAMD_ADDR},inet:${POSTSRSD_HOST}:10001"
+postconf -e "non_smtpd_milters = \$smtpd_milters"
